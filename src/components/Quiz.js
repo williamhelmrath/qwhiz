7ab6f017f2b474822cfc37b5de376558fe07d827
William <william.helmrath@live.com>
2020-12-10 17:06:53 -0500

    implemented personality quiz functionality

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,29 +1,73 @@
 import React, { useState } from "react";
-import { Box, Heading } from "grommet";
+import { Box, Heading, Text } from "grommet";
 
 export default function Quiz({ quiz }) {
+  const [answers, setAnswers] = useState([]);
+  const [result, setResult] = useState();
+
+  const handleSubmit = () => {
+    if (
+      answers.includes(null) ||
+      answers.includes(undefined) ||
+      answers.length !== quiz.questions.length
+    )
+      alert("Please answer all of the questions.");
+    else {
+      const sorted = answers.map((ans) => ans.correspondsTo).sort();
+      console.log(sorted);
+      let mode = sorted[0];
+      let maxOccurences = 1;
+      let currentOccurences = 1;
+      for (let i = 1; i < sorted.length; i++) {
+        if (sorted[i] === sorted[i - 1]) {
+          currentOccurences++;
+        } else {
+          if (currentOccurences > maxOccurences) {
+            maxOccurences = currentOccurences;
+            mode = sorted[i - 1];
+          }
+          currentOccurences = 1;
+        }
+      }
+
+      if (currentOccurences > maxOccurences) {
+        mode = sorted[sorted.length - 1];
+      }
+      quiz.results.forEach((result) => {
+        if (result.text === mode) setResult(result);
+      });
+    }
+  };
+
   if (!quiz.questions) return <div>Error</div>;
   return (
     <Box flex align="center">
       <Heading>{quiz.title}</Heading>
-      {quiz.questions.map((question) => (
-        <Question question={question} key={question.text} />
+      {quiz.questions.map((question, index) => (
+        <Question
+          question={question}
+          key={question.text}
+          index={index}
+          answers={answers}
+          setAnswers={setAnswers}
+        />
       ))}
       <Box
         hoverIndicator
-        onClick={() => {}}
+        onClick={handleSubmit}
         background="lime"
         pad={{ horizontal: "15px" }}
         round="xsmall"
       >
         <Heading>Get score!</Heading>
       </Box>
+      {result && <Result result={result} />}
     </Box>
   );
 }
 
-const Question = ({ question }) => {
-  const [selectedAnswer, setSelectedAnswer] = useState();
+const Question = ({ question, index, answers, setAnswers }) => {
+  // const [selectedAnswer, setSelectedAnswer] = useState();
 
   if (!question.answers) return <div>Error</div>;
 
@@ -35,15 +79,18 @@ const Question = ({ question }) => {
         round="xsmall"
         border={{ color: "border", side: "all", size: "medium" }}
       >
-        <Heading level={2}>{question.text}</Heading>
+        <Heading level={2} textAlign="center">
+          {question.text}
+        </Heading>
       </Box>
       <Box flex direction="row" wrap justify="center">
         {question.answers.map((answer) => (
           <Answer
             answer={answer}
             key={answer.text}
-            selectedAnswer={selectedAnswer}
-            setSelectedAnswer={setSelectedAnswer}
+            answers={answers}
+            setAnswers={setAnswers}
+            index={index}
           />
         ))}
       </Box>
@@ -51,11 +98,12 @@ const Question = ({ question }) => {
   );
 };
 
-const Answer = ({ answer, setSelectedAnswer, selectedAnswer }) => {
-  const handleAnswerClick = () =>
-    selectedAnswer === answer.text
-      ? setSelectedAnswer()
-      : setSelectedAnswer(answer.text);
+const Answer = ({ answer, answers, setAnswers, index }) => {
+  const handleAnswerClick = () => {
+    let temp = [...answers];
+    temp[index] = answers[index] === answer ? null : answer;
+    setAnswers(temp);
+  };
 
   if (!answer.text) return <div>Error</div>;
 
@@ -68,7 +116,7 @@ const Answer = ({ answer, setSelectedAnswer, selectedAnswer }) => {
       round="small"
       margin={{ vertical: "20px", horizontal: "40px" }}
       border={
-        selectedAnswer === answer.text
+        answers[index] === answer
           ? { color: "green", side: "all", size: "medium" }
           : { color: "violet", side: "all", size: "medium" }
       }
@@ -76,6 +124,15 @@ const Answer = ({ answer, setSelectedAnswer, selectedAnswer }) => {
       <Heading level={3} textAlign="center">
         {answer.text}
       </Heading>
+    </Box>
+  );
+};
+
+const Result = ({ result }) => {
+  return (
+    <Box>
+      <Heading level={3}>{`You got: ${result.text}`}</Heading>
+      <Text>{result.description}</Text>
     </Box>
   );
 };
