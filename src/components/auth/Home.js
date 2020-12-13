092817bfa5eb68a712e72d9456f0a6bc4d9ba675
William <william.helmrath@live.com>
2020-12-12 19:15:37 -0500

    removed instance of double header

--- src/components/auth/Home.js
+++ src/components/auth/Home.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { Header as GrommetHeader, Heading, Button } from "grommet";
+import { Heading } from "grommet";
 export default function Home() {
   return (
     <div>
-      <GrommetHeader background="brand">
-        <Heading>Qwhiz</Heading>
-        <div>
-          <Button href="/signup" label="Sign Up" margin={{ right: "2vw" }} />
-          <Button href="/login" label="Log In" />
-        </div>
-      </GrommetHeader>
       <Heading textAlign="center">
         This is the homepage for Qwhiz, a social media platform that promotes
         interaction through quizzes!
